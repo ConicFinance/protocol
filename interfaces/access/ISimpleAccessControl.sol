b3d68c9bceff8b0e1d7bba24545ad87d61693f7e
Wicket Warrick <wicket@conic.finance>
2023-11-29 19:07:09 +0000

    Add more events

--- interfaces/access/ISimpleAccessControl.sol
+++ interfaces/access/ISimpleAccessControl.sol
@@ -2,6 +2,9 @@
 pragma solidity 0.8.17;
 
 interface ISimpleAccessControl {
+    event RoleGranted(bytes32 indexed role, address indexed account);
+    event RoleRevoked(bytes32 indexed role, address indexed account);
+
     function accountsWithRole(bytes32 role) external view returns (address[] memory);
 
     function hasRole(bytes32 role, address account) external view returns (bool);
